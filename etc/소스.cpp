051c620ec5e7fe25fad94795ca207b9344d467c7
peacfeel <peacfeel@gmail.com>
2021-05-19 18:54:04 +0900

    good

--- etc/소스.cpp
+++ etc/소스.cpp
@@ -14,7 +14,7 @@ typedef struct {
 
 int main(void)
 {
-	cout << "This homework is too easy " << endl;
+	cout << "This homework is done of Ver2" << endl;
 	Data D[4] = {};
 
 	for (int i = 0; i < 4; i++) {
